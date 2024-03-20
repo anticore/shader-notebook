23118c804d2c9ec68bc280657e4a579c3191da36
anticore <slimedev@gmail.com>
2024-03-20 21:15:43 +0000

    add multiple shaders

--- src/shaders/index.ts
+++ src/shaders/index.ts
@@ -1,8 +1,13 @@
 import glowBallsFrag from "./glowBalls.glsl";
+import checkerFrag from "./checker.glsl";
 
 export default [
   {
     hash: "glowBalls",
     frag: glowBallsFrag,
+  },
+  {
+    hash: "checker",
+    frag: checkerFrag,
   },
 ];
